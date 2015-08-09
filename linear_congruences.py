c3ea57dbf70dd5cf3ebbcce3d24af51329e9d743
Ini Oguntola <Ini@Inis-MacBook-Pro-2.local>
2015-08-09 18:18:46 -0400

    Fixed gcd function

--- linear_congruences.py
+++ linear_congruences.py
@@ -1,53 +1,54 @@
 def lcm(a, b):
-	return a * b / gcd(a, b)
+        return a * b / gcd(a, b)
  
 def gcd(a, b):
     while b != 0:
         (a, b) = (b, a % b)
     return a
+
  
 def linearCombo(a, b):
-	if (a - b == gcd(a, b)):
-		combo = {a, b}
-    		return combo
-	rem1 = Math.max(a, b)
-	rem2 = Math.min(a, b)
-	firstCoef1 = 1
-	firstCoef2 = 0
-	secondCoef1 = 0
-	secondCoef2 = 1
-	while (rem1 % rem2 != 0):
-		divisor = (long)(rem1 / rem2)
-		tempRem = rem1 - divisor * rem2
-		tempFirstCoef = firstCoef1 - divisor * firstCoef2
-		tempSecondCoef = secondCoef1 - divisor * secondCoef2
-		rem1 = rem2
-		rem2 = tempRem
-		firstCoef1 = firstCoef2
-		firstCoef2 = tempFirstCoef
-		secondCoef1 = secondCoef2
-		secondCoef2 = tempSecondCoef
-	if (a > b):
-		answer = {firstCoef2, secondCoef2};
-		print (a + "(" + answer[0] + ")" + b + "(" + answer[1] + ") = " + gcd(a, b));
-		return answer
-	else:
-		answer = {secondCoef2, firstCoef2}
-		print (a + "(" + answer[0] + ") + " + b + "(" + answer[1] + ") = " + gcd(a, b));
-		return answer
+        if (a - b == gcd(a, b)):
+                combo = {a, b}
+                return combo
+        rem1 = max(a, b)
+        rem2 = min(a, b)
+        firstCoef1 = 1
+        firstCoef2 = 0
+        secondCoef1 = 0
+        secondCoef2 = 1
+        while (rem1 % rem2 != 0):
+                divisor = (long)(rem1 / rem2)
+                tempRem = rem1 - divisor * rem2
+                tempFirstCoef = firstCoef1 - divisor * firstCoef2
+                tempSecondCoef = secondCoef1 - divisor * secondCoef2
+                rem1 = rem2
+                rem2 = tempRem
+                firstCoef1 = firstCoef2
+                firstCoef2 = tempFirstCoef
+                secondCoef1 = secondCoef2
+                secondCoef2 = tempSecondCoef
+        if (a > b):
+                answer = {firstCoef2, secondCoef2};
+                print (a + "(" + answer[0] + ")" + b + "(" + answer[1] + ") = " + gcd(a, b));
+                return answer
+        else:
+                answer = {secondCoef2, firstCoef2}
+                print (a + "(" + answer[0] + ") + " + b + "(" + answer[1] + ") = " + gcd(a, b));
+                return answer
  
 def modInverse(congruence):
-		a = 1
-		while ((congruence[0] * a) % congruence[2] != congruence[1]):
-			a += 1
-		arr = {1, a, congruence[2]};
-		return arr
+                a = 1
+                while ((congruence[0] * a) % congruence[2] != congruence[1]):
+                        a += 1
+                arr = {1, a, congruence[2]};
+                return arr
  
 def incongruentSols(a, b, c):
-	solutions = set()
-	if (b % gcd(a, c) == 0):
-		numSols = gcd(a, c);
-		sol = (c + b * linearCombo(a, c)[0] / numSols) % c;
-		for i in xrange(0, numSols):
-			solutions.add((sol + c * i / numSols) % c)
-	return solutions
+        solutions = set()
+        if (b % gcd(a, c) == 0):
+                numSols = gcd(a, c);
+                sol = (c + b * linearCombo(a, c)[0] / numSols) % c;
+                for i in xrange(0, numSols):
+                        solutions.add((sol + c * i / numSols) % c)
+        return solutions
